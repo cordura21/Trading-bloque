719f5156e7a1d77fac5779355db2aa13f70472fa
cordura21 <cordura21@gmail.com>
2023-02-14 17:32:34 -0300

    Gets data directly from importer
    Also fixed import bug (source column), and added some tickers.

--- R/run data importers.R
+++ R/run data importers.R
@@ -6,13 +6,13 @@ library(tidyr)
 library(lubridate)
 
 # AQR Imports -------------------------------------------------------------
-rets$AQR_factors <- read_excel('data importers/aqr factors/Century of Factor Premia Monthly.xlsx', sheet = 'Century of Factor Premia', skip = 18)
+rets$AQR_factors <- read_excel('data importers/aqr factors/Century of Factor Premia Monthly.xlsx', sheet = 'Century of Factor Premia', skip = 18) %>% na.omit()
 names(rets$AQR_factors)[1] <- 'date'
 rets$AQR_factors$date <- as.Date(rets$AQR_factors$date)
 rets$AQR_factors <- pivot_longer(rets$AQR_factors,!date)
 rets$AQR_factors$source <- 'aqr factors'
 
-rets$AQR_TSM <- read_excel('data importers/aqr time series momentum/Time Series Momentum Factors Monthly.xlsx', sheet = 'TSMOM Factors', skip = 17)
+rets$AQR_TSM <- read_excel('data importers/aqr time series momentum/Time Series Momentum Factors Monthly.xlsx', sheet = 'TSMOM Factors', skip = 17) %>% na.omit()
 names(rets$AQR_TSM)[1] <- 'date'
 rets$AQR_TSM$date <- as.Date(rets$AQR_TSM$date)
 rets$AQR_TSM <- pivot_longer(rets$AQR_TSM,!date)
--- R/startup.R
+++ R/startup.R
@@ -18,19 +18,21 @@ source('R/backtest function.R')
 # Read data and params ---------------------------------------------------------
 
 params <- read_yaml('portafolios.yml')
-funds <- read_excel(path = "data/returns.xlsx", sheet = 'returns')
+funds <- read.csv(file  = 'data importers/returns.csv') %>% mutate(date = as.Date(date))
 
 #force dates to end of month dates. This is useful if you have for example
 # 1970-12-01 and 1970-12-31 in the same file, because you joined files with
 # multiple monthly formats.
 
 if(params$global.settings$force.end.of.months == TRUE){
-  funds$date <- lubridate::ceiling_date(funds$date, "month") - 1 %>% as.Date()
+  funds$date <- lubridate::ceiling_date(funds$date, "month") - 1 
 }
 
-funds <- funds %>% pivot_wider(names_from = name, values_from = value) %>%
+funds <- funds %>% select(date,name,value) %>%
+  pivot_wider(names_from = name, values_from = value) %>%
   mutate(date = as.Date(date))
 funds$Uninvested <- 0
 names(funds)[1] <- 'date'
 
 funds <- xts(funds[,-1],funds$date)
+
