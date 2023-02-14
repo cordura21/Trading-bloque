a10cea2fe03f998b788ac7437dae97a2846704d5
cordura21 <cordura21@gmail.com>
2023-02-14 17:40:44 -0300

    Chesapeake Mutual Fund analysis

--- R/run data importers.R
+++ R/run data importers.R
@@ -6,13 +6,13 @@ library(tidyr)
 library(lubridate)
 
 # AQR Imports -------------------------------------------------------------
-rets$AQR_factors <- read_excel('data importers/aqr factors/Century of Factor Premia Monthly.xlsx', sheet = 'Century of Factor Premia', skip = 18) %>% na.omit()
+rets$AQR_factors <- read_excel('data importers/aqr factors/Century of Factor Premia Monthly.xlsx', sheet = 'Century of Factor Premia', skip = 18) 
 names(rets$AQR_factors)[1] <- 'date'
 rets$AQR_factors$date <- as.Date(rets$AQR_factors$date)
 rets$AQR_factors <- pivot_longer(rets$AQR_factors,!date)
 rets$AQR_factors$source <- 'aqr factors'
 
-rets$AQR_TSM <- read_excel('data importers/aqr time series momentum/Time Series Momentum Factors Monthly.xlsx', sheet = 'TSMOM Factors', skip = 17) %>% na.omit()
+rets$AQR_TSM <- read_excel('data importers/aqr time series momentum/Time Series Momentum Factors Monthly.xlsx', sheet = 'TSMOM Factors', skip = 17)
 names(rets$AQR_TSM)[1] <- 'date'
 rets$AQR_TSM$date <- as.Date(rets$AQR_TSM$date)
 rets$AQR_TSM <- pivot_longer(rets$AQR_TSM,!date)
@@ -55,5 +55,5 @@ rets$csv_data <- csv_data %>% as_tibble() %>%
   mutate(date = as.Date(date),name = as.character(name),source = as.character(source))
 
 # Join all inputs and save ------------------------------------------------
-output_data <- bind_rows(rets)
+output_data <- bind_rows(rets) %>% na.omit()
 write.csv(output_data,'data importers/returns.csv', row.names = FALSE)
