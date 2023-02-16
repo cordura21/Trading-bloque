7c4b3b67d451f562e059fb7164bd436f4971a308
NicksLG <frontoff16@gmail.com>
2023-02-16 11:06:34 -0300

    pasé la reactable (summary_table) a la parte del .RMD

--- R/tables.R
+++ R/tables.R
@@ -1,16 +1,5 @@
 tables <- list()
 tables$summary_stats <- pivot_wider(case_stats(portfolios),names_from = variable)
-summary_table <- reactable::reactable(tables$summary_stats
-                     ,outlined = FALSE,highlight = TRUE
-                     ,columns = list(
-                       portfolio = colDef(minWidth = 120)
-                       ,Growth = colDef(format = colFormat(digits = 1,locales = "en-US",prefix  = 'x'))
-                       ,CAGR = colDef(format = colFormat(percent = TRUE, digits = 2,locales = "en-US"))
-                       ,maxDD = colDef(format = colFormat(percent = TRUE, digits = 1,locales = "en-US"))
-                       ,Vol = colDef(format = colFormat(percent = TRUE, digits = 1,locales = "en-US"))
-                       , Sharpe = colDef(format = colFormat(digits = 1,locales = "en-US"))
-                       , Calmar = colDef(format = colFormat(digits = 1,locales = "en-US"))))
-
 
 investable_assets <- case_returns[,!(names(case_returns) %in% "Uninvested")]
 
