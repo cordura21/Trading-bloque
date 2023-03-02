bcd8b5bbf8d9968403bfa94e1fd35dd768e3f72d
NicksLG <frontoff16@gmail.com>
2023-03-02 16:15:51 -0300

    cambios en visualizacion si se muestra con misma vola
    Cuando se corre con equal vol:
    EL nombre del output generado empieza con VOL:_ADJUSTED
    La columna de volatilidad está pintada

--- R/report.R
+++ R/report.R
@@ -1,3 +1,13 @@
-report_path <- file.path('results',paste0(case_to_run,' backtest.html'))
+if(vol_adjust == 1){
+  
+  name_case_to_run = paste0('VOL_ADJUSTED ',case_to_run)
+  
+}else{
+  
+  name_case_to_run  = case_to_run
+  
+}
+
+report_path <- file.path('results',paste0(name_case_to_run,' backtest.html'))
 
 rmarkdown::render('report.Rmd', output_file = report_path)
